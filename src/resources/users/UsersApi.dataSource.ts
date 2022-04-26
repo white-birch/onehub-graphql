3464667c19bf9b1b6a43c9cdc7a3b2cc59334cd9
Scott Schmalz <scott.schmalz@gmail.com>
2022-04-26 14:35:47 -0400

    add sign in operation

--- src/resources/users/UsersApi.dataSource.ts
+++ src/resources/users/UsersApi.dataSource.ts
@@ -3,6 +3,10 @@ import _OneHubApi from '../../utils/_OneHubApi';
 import type { CreateUserInput, User } from './users.types';
 
 class UsersApi extends _OneHubApi {
+  async signIn(email: string, password: string): Promise<{ token: string; user: User }> {
+    return this.post('/auth/sign-in', { email, password });
+  }
+
   async signUp(data: CreateUserInput): Promise<{ token: string; user: User }> {
     return this.post('/auth/sign-up', data);
   }
---
+++ src/resources/users/users.resolvers.ts
@@ -0,0 +1,11 @@
+import type { Context } from '../../context.types';
+
+const signIn = async (parent: undefined, { email, password }: { email: string; password: string }, context: Context): Promise<string> => {
+  const { token } = await context.dataSources.usersApi.signIn(email, password);
+  context.token = token;
+  return token;
+};
+
+export default {
+  Mutation: { signIn },
+};
