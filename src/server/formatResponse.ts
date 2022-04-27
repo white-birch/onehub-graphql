98e3189e78747022746fb531126f216e9b04760f
Scott Schmalz <scott.schmalz@gmail.com>
2022-04-26 22:54:15 -0400

    adjust error response

--- src/server/formatResponse.ts
+++ src/server/formatResponse.ts
@@ -1,6 +1,5 @@
 import { Config } from 'apollo-server-cloud-functions';
 import { ExpressContext } from 'apollo-server-express';
-import { GraphQLResponse } from 'apollo-server-types';
 import { get } from 'lodash';
 import logger from '../utils/logger';
 
@@ -8,14 +7,14 @@ const formatResponse: Config<ExpressContext>['formatResponse'] = (response) => {
   if (!response.errors) return response;
 
   const errors = response.errors.map((error) => {
-    const code = error.extensions?.code || 'INTERNAL_SERVER_ERROR';
+    const code = (error.extensions?.code || 'INTERNAL_SERVER_ERROR') as string;
     const errors = get(error, 'extensions.exception.errors');
-    return { code, errors };
+    return { message: 'An error occurred while processing your request.', extensions: { code, errors } };
   });
 
   logger.error({ message: 'Returning error(s) in response', errors: { raw: response.errors, normalized: errors } });
 
-  return { ...response, errors } as unknown as GraphQLResponse;
+  return { ...response, errors };
 };
 
 export default formatResponse;
