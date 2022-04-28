92f4b66a79dca95c3f31cb4530db28e5cd130fc5
Scott Schmalz <scott.schmalz@gmail.com>
2022-04-28 16:30:20 -0400

    adjust user mutations

--- src/resources/users/users.resolvers.ts
+++ src/resources/users/users.resolvers.ts
@@ -2,15 +2,10 @@ import { decode, deleteToken, getToken, setToken } from '../../utils/token';
 
 import type { Context } from 'server/context';
 
-const getMe = (context: Context) => {
-  const token = getToken(context);
-  return token ? { token, ...decode(token) } : undefined;
-};
-
 const signIn = async (parent: undefined, { email, password }: { email: string; password: string }, context: Context) => {
   const { token } = await context.dataSources.usersApi.signIn(email, password);
   setToken(token, context);
-  return { token, ...decode(token) };
+  return { token };
 };
 
 const signOut = (parent: undefined, args: undefined, context: Context) => {
@@ -18,7 +13,10 @@ const signOut = (parent: undefined, args: undefined, context: Context) => {
   return true;
 };
 
-const me = (parent: undefined, args: undefined, context: Context) => getMe(context);
+const me = (parent: undefined, args: undefined, context: Context) => {
+  const token = getToken(context);
+  return token ? { token, ...decode(token) } : undefined;
+};
 
 export default {
   Mutation: { signIn, signOut },
